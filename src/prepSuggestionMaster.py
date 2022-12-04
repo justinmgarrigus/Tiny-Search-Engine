25bb97ded1f602d7a00dd54ae40542c93bcf0f05
Saloonster <97369256+Saloonster@users.noreply.github.com>
2022-12-03 18:21:35 -0600

    Removing print test statements and adding comments
    Improved comments for readability and detail.

--- src/prepSuggestionMaster.py
+++ src/prepSuggestionMaster.py
@@ -11,32 +11,28 @@ def appendPrepSuggestionMaster(): #this should be used in most cases for initial
   }
 
   suggestionSeeds = open("/data/suggestionSeeds.txt").read().splitlines()   #opening and reading the suggestion seeds
-  cachedPlace = open("/data/lastSeed.txt").read().splitlines()
-  print(cachedPlace[0])
-  appendSuggestions = []
-  i = len(suggestionSeeds) - 1
-  k = int(cachedPlace[1])
-  cacheLine = str(i)
-  tempItem = suggestionSeeds[i]
-  if k != i:
-      f = open('/data/lastSeed.txt', 'w')
+  cachedPlace = open("/data/lastSeed.txt").read().splitlines()              #grabbing a list of all the text of the last seed and its line number
+  appendSuggestions = []                                                    #temporary variable
+  i = len(suggestionSeeds) - 1                                              #line number of actual end of seeds
+  k = int(cachedPlace[1])                                                   #cached line number
+  cacheLine = str(i)                                                        #casting line number to string for writing to cache text file
+  tempItem = suggestionSeeds[i]                                             #working backwards from end
+  if k != i:                                                                #comparing current line to cached line number
+      f = open('/data/lastSeed.txt', 'w')                                   #If they are not the same, then you need to append elaborations, and cache new final line in file
       f.write(tempItem + "\n")
       a = open('/data/lastSeed.txt', 'a')
       a.writelines(cacheLine)
-      while i >= k:
+      while i >= k:                                                         #creating a small list of new seeds
         tempItem = suggestionSeeds[i]
         appendSuggestions.append(tempItem)
         i = i - 1
 
   fout = open('/data/suggestionMaster.csv', 'a', newline="")                            #openning the master suggestion csv
   suggestionCSVWriter = csv.writer(fout)    #prepping the csv writer
-  print(appendSuggestions)
-  print("\n")
   #iterating through each suggestion seed
-  if len(appendSuggestions) != 0:
-    for seed in appendSuggestions: 
+  if len(appendSuggestions) != 0:                                         #checking to see if new seeds appeared
+    for seed in appendSuggestions:                                        #elaborating and appending to CSV
       response = requests.get('http://google.com/complete/search?client=chrome&q=' + seed, headers=headers)   #searching to elaborate seed                                #appending to temporary row
-      print(json.loads(response.text)[1])
       suggestionCSVWriter.writerow(json.loads(response.text)[1])
 
 def resetPrepSuggestionMaster():
@@ -64,7 +60,7 @@ def searchSuggestionDriver(partialSearch): #pass the string without the suffix '
       "User-Agent":
       "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36 Edge/18.19582"
     }
-    response = requests.get('http://google.com/complete/search?client=chrome&q=' + normalizedSearch, headers=headers)   #searching to elaborate seed
+    response = requests.get('http://google.com/complete/search?client=chrome&q=' + normalizedSearch, headers=headers)   #elaborate seed
     unseenSuggestions = json.loads(response.text)[1]
 
     return unseenSuggestions[:_MAX_RESPONSES]
